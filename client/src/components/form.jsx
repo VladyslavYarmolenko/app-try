bd3ec2f2abc0778f70540f7b087d8fdf31b64fb7
Dmytro Parzhytskyi <dmytro_parzhytskyi@epam.com>
2021-02-27 11:39:23 +0200

    Send POST /users

--- client/src/components/form.jsx
+++ client/src/components/form.jsx
@@ -1,11 +1,40 @@
 import { useState, /* useEffect */ } from "react";
 
 export default function Form() {
+	const [ userID, setUserID ] = useState("");
 	const [ name, setName ] = useState("");
+	const [ sending, setSending ] = useState(false);
 
 	return (
 		<>
-			<form>
+			<p>
+				{userID ? (
+					<span>Created user {userID}</span>
+					) : (
+					<span>No users created yet</span>
+				)}
+			</p>
+
+			<form
+				onSubmit={async (event) => {
+					event.preventDefault();
+
+					if (!name)
+						return;
+
+					setSending(true);
+
+					const response = await fetch("http://localhost:8081/users", {
+						method: "POST",
+						body: { name },
+					});
+
+					const { result: id } = await response.json();
+
+					setUserID(id);
+					setSending(false);
+				}}
+			>
 				<p>
 					<span>Name:</span>
 					<input
@@ -18,7 +47,10 @@ export default function Form() {
 					/>
 				</p>
 
-				<button type="submit">Send</button>
+				<button
+					type="submit"
+					disabled={sending}
+				>Send</button>
 			</form>
 
 			<p>{name}</p>
