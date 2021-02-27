c955683d3a4d03cfe73323dbab6f3964fc83a5eb
Dmytro Parzhytskyi <dmytro_parzhytskyi@epam.com>
2021-02-27 11:56:10 +0200

    Fix missing next() call in allowOrigin

--- server/src/router/allow-origin.js
+++ server/src/router/allow-origin.js
@@ -1,7 +1,9 @@
-const allowOrigin = (origin) => (req, res) => {
+/** @returns {import("express").RequestHandler} */
+const allowOrigin = (origin) => (req, res, next) => {
 	res.header({
 		"Access-Control-Allow-Origin": origin,
 	});
+	next();
 };
 
 module.exports = allowOrigin;
